4b228af13a33c808bdafa791107d8332ac2c7f77
juanantoniogit <juanantonio.tecno@gmail.com>
2023-03-23 11:11:29 +0100

    Add files via upload

---
+++ Juego/Escenarios/Potenciadores/Puntos.gd
@@ -0,0 +1,18 @@
+extends Node2D
+#var puntos = $"/root/Global".puntos
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+#	puntos = puntos + 10
+#	print (puntos)
+	pass # Replace with function body.
